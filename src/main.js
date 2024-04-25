78867a979311b9dbed010451d38129fc81d363c6
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-04-25 17:47:33 +0200

    Messagepage route toegevoegd

--- src/main.js
+++ src/main.js
@@ -5,10 +5,12 @@ import App from './App.vue'
 
 import HomePage from './pages/HomePage.vue';
 import TripPage from './pages/TripPage.vue';
+import MessagePage from './pages/MessagePage.vue';
 
 const routes = [
     { path: '/', component: HomePage },
     { path: '/trip', component: TripPage },
+    { path: '/messages/:id', component: MessagePage }
 ]
 
 const router = createRouter({
