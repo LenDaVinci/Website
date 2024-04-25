5bfe53834a845d3bf46299a6e5cadca98672f8dc
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-04-25 17:48:17 +0200

    Tekstuitlijning op rechts zodat het makkelijker leest

--- src/pages/TripPage.vue
+++ src/pages/TripPage.vue
@@ -4,7 +4,7 @@
     <div class="bg-gentimg bg-fixed bg-[center_top] bg-cover h-tripimg p-8"> 
     </div> 
         <h1 class="text-3xl text-black text-center">Dagje Gent (hoewel Zwolle ook heel ziek is)</h1>
-        <p class="mx-48 text-center my-8 text-black">
+        <p class="mx-48 my-8 text-black">
             Dit is een heel lang verhaaltje, maar het is wel een verhaaltje. Over het dagje, het dagje wat jullie gaan hebben samen naar Gent (ofzo). Het is te lang om uit te schrijven dus hier komt een lorem ipsumpje; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet iaculis turpis, sed rutrum turpis egestas a. Nunc in pulvinar neque. 
             Vivamus efficitur lacinia purus ut efficitur. Curabitur auctor tempus augue. Duis placerat erat eget pretium ornare. Aliquam vel lectus sed ligula sagittis posuere. In hac habitasse platea dictumst. 
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quia excepturi consectetur consequatur nesciunt, ea veniam, corporis fugiat alias eum harum, commodi recusandae tempore nobis enim atque omnis dicta architecto?
