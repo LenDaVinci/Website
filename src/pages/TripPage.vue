8429334fc7c18bc96871c01612a17a480b8f10f6
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-04-25 22:37:13 +0200

    Formatting

--- src/pages/TripPage.vue
+++ src/pages/TripPage.vue
@@ -3,19 +3,18 @@
 <template>
     <div class="bg-gentimg bg-fixed bg-[center_top] bg-cover h-tripimg p-8"> 
     </div> 
-        <h1 class="text-3xl text-black text-center">Dagje Gent (hoewel Zwolle ook heel ziek is)</h1>
-        <p class="mx-48 my-8 text-black font-glacialIndifference">
-            Dit is een heel lang verhaaltje, maar het is wel een verhaaltje. Over het dagje, het dagje wat jullie gaan hebben samen naar Gent (ofzo). Het is te lang om uit te schrijven dus hier komt een lorem ipsumpje; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet iaculis turpis, sed rutrum turpis egestas a. Nunc in pulvinar neque. 
-            Vivamus efficitur lacinia purus ut efficitur. Curabitur auctor tempus augue. Duis placerat erat eget pretium ornare. Aliquam vel lectus sed ligula sagittis posuere. In hac habitasse platea dictumst. 
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quia excepturi consectetur consequatur nesciunt, ea veniam, corporis fugiat alias eum harum, commodi recusandae tempore nobis enim atque omnis dicta architecto?
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, magnam! Optio adipisci beatae sequi aut perferendis ratione aliquam illo consequuntur iste consectetur deserunt fugit ea, similique nemo delectus nulla laboriosam?
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis velit impedit necessitatibus, eligendi iste voluptatem sapiente veniam at! Adipisci laudantium optio est voluptatibus tempora mollitia aperiam dolorem eos molestias quis.<br><br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quia excepturi consectetur consequatur nesciunt, ea veniam, corporis fugiat alias eum harum, commodi recusandae tempore nobis enim atque omnis dicta architecto?
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis numquam ducimus quidem suscipit deserunt fugit nobis ullam voluptate cupiditate cumque tenetur repellat maiores, rerum nihil. Cupiditate deleniti aperiam distinctio fugiat!
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis numquam ducimus quidem suscipit deserunt fugit nobis ullam voluptate cupiditate cumque tenetur repellat maiores, rerum nihil. Cupiditate deleniti aperiam distinctio fugiat!
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi ex soluta saepe molestias fugiat dolorem sequi laudantium vitae commodi ut autem, maxime, natus assumenda! Id vitae incidunt sequi consectetur rem.<br><br>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis, quidem possimus aspernatur dolorem sed animi delectus tempore cumque obcaecati optio rem odio exercitationem, consequatur ipsam nisi quibusdam. Ipsum, eum.
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis, quidem possimus aspernatur dolorem sed animi delectus tempore cumque obcaecati optio rem odio exercitationem, consequatur ipsam nisi quibusdam. Ipsum, eum.
-        </p>
- 
+    <h1 class="text-3xl text-black text-center">Dagje Gent (hoewel Zwolle ook heel ziek is)</h1>
+    <p class="mx-48 my-8 text-black font-glacialIndifference">
+        Dit is een heel lang verhaaltje, maar het is wel een verhaaltje. Over het dagje, het dagje wat jullie gaan hebben samen naar Gent (ofzo). Het is te lang om uit te schrijven dus hier komt een lorem ipsumpje; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet iaculis turpis, sed rutrum turpis egestas a. Nunc in pulvinar neque. 
+        Vivamus efficitur lacinia purus ut efficitur. Curabitur auctor tempus augue. Duis placerat erat eget pretium ornare. Aliquam vel lectus sed ligula sagittis posuere. In hac habitasse platea dictumst. 
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quia excepturi consectetur consequatur nesciunt, ea veniam, corporis fugiat alias eum harum, commodi recusandae tempore nobis enim atque omnis dicta architecto?
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Praesentium, magnam! Optio adipisci beatae sequi aut perferendis ratione aliquam illo consequuntur iste consectetur deserunt fugit ea, similique nemo delectus nulla laboriosam?
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis velit impedit necessitatibus, eligendi iste voluptatem sapiente veniam at! Adipisci laudantium optio est voluptatibus tempora mollitia aperiam dolorem eos molestias quis.<br><br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui quia excepturi consectetur consequatur nesciunt, ea veniam, corporis fugiat alias eum harum, commodi recusandae tempore nobis enim atque omnis dicta architecto?
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis numquam ducimus quidem suscipit deserunt fugit nobis ullam voluptate cupiditate cumque tenetur repellat maiores, rerum nihil. Cupiditate deleniti aperiam distinctio fugiat!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis numquam ducimus quidem suscipit deserunt fugit nobis ullam voluptate cupiditate cumque tenetur repellat maiores, rerum nihil. Cupiditate deleniti aperiam distinctio fugiat!
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eligendi ex soluta saepe molestias fugiat dolorem sequi laudantium vitae commodi ut autem, maxime, natus assumenda! Id vitae incidunt sequi consectetur rem.<br><br>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis, quidem possimus aspernatur dolorem sed animi delectus tempore cumque obcaecati optio rem odio exercitationem, consequatur ipsam nisi quibusdam. Ipsum, eum.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt perspiciatis, quidem possimus aspernatur dolorem sed animi delectus tempore cumque obcaecati optio rem odio exercitationem, consequatur ipsam nisi quibusdam. Ipsum, eum.
+    </p>
 </template>
