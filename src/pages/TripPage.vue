e395abdc94b60f78a8f76f91b434bfa7a3e8d4fa
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-04-27 01:41:05 +0200

    Text markup

--- src/pages/TripPage.vue
+++ src/pages/TripPage.vue
@@ -1,18 +1,18 @@
 <script setup>
 </script>
 <template>
-    <div class="bg-gentimg bg-fixed bg-[center_top] bg-cover h-tripimg p-8"> 
+    <div class="bg-gentimg bg-fixed bg-center bg-cover h-tripimg p-8">  
     </div> 
     <h1 class="text-3xl text-black text-center">Dagje Gent</h1>
     <p class="mx-48 my-8 text-black font-glacialIndifference">
-        Pau,
-        Op deze speciale dag wil ik je verrassen met een dagje weg naar Gent, bekend om zijn charme, romantiek en bloemen!
-        De bloemenmarkt van Gent tussen de middeleeuwse gebouwen is een prachtige combinatie van kleur en vrolijkheid en dat past helemaal bij jou!
-        In Gent zijn er ook genoeg leuke activiteiten te doen, van rondjes lopen door de oude stad, varen over de grachten, en shoppen in de gezellige winkelstraat, wat bij jou wel goed komt!
-        Daarnaast staat Gent ook bekend om de heerlijke culinaire gerechten die je daar kan eten, zoals de klassieke Vlaamse gerechten, Gentse Neuzen, maar ook een heerlijke stoverij (stoofvlees).
-        Zou jij een dagje met mij mee willen naar deze gezellige stad, waar we een gezellig dagje weggaan, shoppen en lekker eten?
-        Liefs,
-        Len
+        Pau,<br>
+        Op deze speciale dag wil ik je verrassen met een dagje weg naar Gent, bekend om zijn charme, romantiek en bloemen!<br>
+        De bloemenmarkt van Gent tussen de middeleeuwse gebouwen is een prachtige combinatie van kleur en vrolijkheid en dat past helemaal bij jou!<br>
+        In Gent zijn er ook genoeg leuke activiteiten te doen, van rondjes lopen door de oude stad, varen over de grachten, en shoppen in de gezellige winkelstraat, wat bij jou wel goed komt!<br>
+        Daarnaast staat Gent ook bekend om de heerlijke culinaire gerechten die je daar kan eten, zoals de klassieke Vlaamse gerechten, Gentse Neuzen, maar ook een heerlijke stoverij (stoofvlees).<br>
+        Zou jij een dagje met mij mee willen naar deze gezellige stad, waar we een gezellig dagje weggaan, shoppen en lekker eten?<br>
+        Liefs,<br>
+        Len<br>
         PS. Ik hoop dan dat we samen met de auto kunnen, maar misschien moet jij rijden :p
     </p>
 </template>
