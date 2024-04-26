89d838d3b357c882940774bb4f159771f16f4566
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-04-27 01:28:31 +0200

    Final text

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -2,11 +2,13 @@
 </script>
 <template>
     <div class="bg-bgimg bg-cover min-h-[calc(100vh-3rem)] p-8"> 
-    <h1 class="text-3xl text-textcolor text-center">Happy Birthday!</h1>
-    <p class="mx-48 text-center my-8 text-textcolor font-glacialIndifference">
-      Hier komt dan een beginnend verhaaltje van Lennart, we kunnen bijvoorbeeld verwijzen naar de andere pagina waar het cadeautje te vinden is, komt later allemaal.
-      In ieder geval moet dit een beetje lang zijn voor de vorm, dus let op, hier volgt een mini lorem ipsumpje; Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam aliquet iaculis turpis, sed rutrum turpis egestas a. Nunc in pulvinar neque. 
-      Vivamus efficitur lacinia purus ut efficitur. Curabitur auctor tempus augue. Duis placerat erat eget pretium ornare. Aliquam vel lectus sed ligula sagittis posuere. In hac habitasse platea dictumst. 
+    <h1 class="text-3xl text-textcolor text-center">Gefeliciteerd Pauline!</h1>
+    <p class="mx-48 my-8 text-white font-glacialIndifference">
+      Lieve Pauline, <br>
+      Je naam betekent klein of bescheiden, maar dat ben jij zeker niet, voornamelijk met de gezelligheid die jij met je meebrengt! Iedere dag ben jij weer het zonnetje in Dirksland ;p<br>
+      Vandaag ben je 18 jaar geworden, volwassen. Hier twijfelen de meeste nog wel aan, maar toch gaat er begonnen worden aan een rijbewijs en wijnen.<br>
+      We wensen jou daarom ook allemaal een hele fijne verjaardag en nog vele jaren waarin we allemaal mogen genieten van jou gezelligheid en geplaag!<br>
+      Ga maar snel alle berichtjes lezen die iedereen voor jou geschreven heeft, inclusief mooie herinneringen aan en van jou! 
     </p>
   </div>
 </template>
