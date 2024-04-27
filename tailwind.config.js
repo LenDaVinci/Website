f23ed6a076f3439f7488d5a4c29a6d53b8e23f80
Sjoerd <sjoerdhoogvliet@gmail.com>
2024-04-27 14:59:26 +0200

    Set the goalposts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   content: ['./index.html', './src/main.js', "./src/**/*.{vue,js,ts,jsx,tsx}",],
   theme:  {
     screens: {
-      'm': '768px',
+      'm': '968px',
       
     },
       extend: {
